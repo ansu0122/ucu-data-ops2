2462e38578ecb9648cec1ae60adda461b8baed36
Andriy Sukh <asukh@softserveinc.com>
2024-01-07 01:11:59 +0200

    adding db auth

--- flask-redis/app.py
+++ flask-redis/app.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from redis import Redis
+import os
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+with open('/run/secrets/redis-password', 'r') as file:
+    redis_password = file.read().strip()
+
+redis = Redis(host='redis', port=int(os.environ.get(
+    'REDIS_PORT', 6379)), password=redis_password)
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
+    counter = str(redis.get('hits'), 'utf-8')
     return "This webpage has been viewed "+counter+" time(s)"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get(
+        'FLASK_PORT', 8050)), debug=True)
